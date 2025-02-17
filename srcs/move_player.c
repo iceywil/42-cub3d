60a0403fb2b1aba5678c29e1b1595bd4f461c31c
Kim Nguyen <kimnguye@bess-f2r1s2.clusters.42paris.fr>
2025-02-17 16:55:52 +0100

    field of view of mini map : ok

--- srcs/move_player.c
+++ srcs/move_player.c
@@ -6,7 +6,7 @@
 /*   By: kimnguye <kimnguye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 16:11:07 by kimnguye          #+#    #+#             */
-/*   Updated: 2025/02/17 16:35:03 by kimnguye         ###   ########.fr       */
+/*   Updated: 2025/02/17 16:54:25 by kimnguye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ float	new_y(t_player *player)
 		return (y - sin(player->angle) * SPEED);
 	if (player->key_right)
 		return (y + cos(player->angle) * SPEED);
-	return (player->y);
+	return (y);
 }
 
 float	new_x(t_player *player)
@@ -41,12 +41,10 @@ float	new_x(t_player *player)
 		return (x - cos(player->angle) * SPEED);
 	if (player->key_right)
 		return (x - sin(player->angle) * SPEED);
-	return (player->x);
+	return (x);
 }
-// implement walls blocking movements ? Check if Pos == '1',
-// if true set pos to wall pos
 
-void	move_player(t_player *player, t_cub *cub)
+void	update_angle(t_player *player)
 {
 	if (player->left_rotate)
 		player->angle -= ROT_SPEED;
@@ -56,32 +54,43 @@ void	move_player(t_player *player, t_cub *cub)
 		player->angle -= 2 * PI;
 	else if (player->angle < - 2 * PI)
 		player->angle += 2 * PI;
-	// fix N Player
-	/* 	if (player->angle < 0)
-			player->angle = 2 * PI; */
+}
+// implement walls blocking movements ? Check if Pos == '1',
+// if true set pos to wall pos
+
+void	move_player(t_player *player, t_cub *cub)
+{
 	float	x;
 	float	y;
-
+	
+	update_angle(player);
 	x = new_x(player);
 	y = new_y(player);
 	ft_printf("old map[%i][%i]=%c\n", (int) player->y / BLOCK, (int)player->x / BLOCK, cub->map[(int) player->y / BLOCK][(int)player->x / BLOCK]);
-	ft_printf("new map[%i][%i]=%c\n", (int) y / BLOCK, (int)x / BLOCK, cub->map[(int)y / BLOCK][(int)x / BLOCK]);
-	// if ((y >= 0 && x >= 0 && cub->map[(int)y / BLOCK][(int)x / BLOCK] != '1'))
-	// {
-	// 	player->x = x;
-	// 	player->y = y;
-	// }
-	if (y < 0 || x < 0)
-		return ;
+	ft_printf("new map[%i][%i]=%c\n", (int)y / BLOCK, (int)x / BLOCK, cub->map[(int)y / BLOCK][(int)x / BLOCK]);
+	printf("old pos=(%f,%f)\n", player->x, player->y);
+	printf("new pos=(%f,%f)\n", x, y);
 	if (x > player->x || y > player->y)
 	{	
+		printf("cas 1\n");
 		if (cub->map[(int)(y + PLAYER_SIZ)/ BLOCK][(int)(x + PLAYER_SIZ)/ BLOCK] == '1')
+			return ;
+	}
+	else if (x < player->x)
+	{
+		printf("cas 2\n");
+		if (cub->map[(int)y/ BLOCK][(int)(x - PLAYER_SIZ)/ BLOCK] == '1')
 			return ;
 	}
 	else 
 	{
+		ft_printf("cas 3: ");
+		ft_printf("new map[%i][%i]=%c\n", (int)y / BLOCK, (int)x / BLOCK, cub->map[(int)y / BLOCK][(int)x / BLOCK]);
 		if (cub->map[(int)y / BLOCK][(int)x / BLOCK] == '1')
+		{
+			ft_printf("CUL DE SAC\n");
 			return ;
+		}
 	}
 	player->x = x;
 	player->y = y;
